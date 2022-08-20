2f6fe333617e2e075fab9f1a1eff1f2c523732c3
victorFloresDubon <vdubon@outlook.com>
2022-08-20 09:04:43 -0600

    Se agrega carpeta build para publicación en git

--- src/components/inicio/estadios.jsx
+++ src/components/inicio/estadios.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { child, get, ref } from "firebase/database";
+import React, { useEffect, useState } from "react";
+import { db } from "../../firebase/firebase";
 
 export default function Estadios() {
+
+    const path = 'Estadios';
+    const [estadiosData, setEstadiosData] = useState([]);
+
+    useEffect(() => {
+        let dbRef = ref(db);
+        let estadios = [];
+        get(child(dbRef, path)).then((snapshot) => {
+            if (snapshot.exists()){
+                snapshot.forEach((estadioSnapshot) => {
+                    estadios.push(estadioSnapshot);
+                })
+            }
+            setEstadiosData(estadios);
+        });
+
+        return () => {
+            setEstadiosData([]);
+        }
+    }, []);
+
+
+
     return (
-        <div> Acá irán los estadios</div>
+        <div className="bg-corinto text-blanco">
+            Acá irán los estadios
+        </div>
     );
 }
