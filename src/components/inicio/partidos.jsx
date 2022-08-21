208b573716b1777ab49fe8675fdd18c778f2e35a
victorFloresDubon <vdubon@outlook.com>
2022-08-20 18:45:12 -0600

    Nuevos partidos

--- src/components/inicio/partidos.jsx
+++ src/components/inicio/partidos.jsx
@@ -2,10 +2,6 @@ import React from "react";
 
 export default function Partidos(){
     return(
-<<<<<<< HEAD
-        <div>Acá irán los partidos</div>
-=======
         <div>Acá irán los partidos<br/>...</div>
->>>>>>> fd523ba6fa15b7cd81a9720097e03019c963b6d9
     );
 }
