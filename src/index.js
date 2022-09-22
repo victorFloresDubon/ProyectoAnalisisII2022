86d30156286ec556bef381a6a27e7bd697eb4d4c
victorFloresDubon <vdubon@outlook.com>
2022-09-22 06:10:29 -0600

    Se agrega servicios para conectar con API

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './tailwind.output.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Registro from './components/login/Registro';
 import Login from './components/login/Login';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <Router>
     <Routes>
       <Route path='/' element={<App/>}/>
       <Route path='/registro' element={<Registro/>}/>
       <Route path='/login' element={<Login/>}/>
     </Routes>
-  </BrowserRouter>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
