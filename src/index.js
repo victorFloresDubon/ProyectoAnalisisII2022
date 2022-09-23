af9743d07da77546e3db8dc3a5a7c41ac8b17820
victorFloresDubon <vdubon@outlook.com>
2022-09-22 18:23:52 -0600

    Se ordena el ruteador con hashbrowser

--- src/index.js
+++ src/index.js
@@ -2,25 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './tailwind.output.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Registro from './components/login/Registro';
-import Login from './components/login/Login';
+import {HashRouter as Router} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router basename={process.env.REACT_APP_BASENAME}>
-    <Routes>
-      <Route path='/' element={<App/>}/>
-      <Route path='/registro' element={<Registro/>}/>
-      <Route path='/login' element={<Login/>}/>
-    </Routes>
-  </Router>
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
